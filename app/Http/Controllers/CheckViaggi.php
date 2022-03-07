ea7a6ffcaa68068675b2c53acaf7f0a6b6a9f954
LorenzoBernardini88 <lorenzo.bernardini88@gmail.com>
2022-03-07 15:23:57 +0100

    first commit

---
+++ app/Http/Controllers/CheckViaggi.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class checkViaggi extends Controller
+{
+    public function index(){
+        return view('home');
+    }
+}
